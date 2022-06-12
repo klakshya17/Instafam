a746ce6899b9c1ae54b13f156f5ecbef69e875ae
Kumar Lakshya <klakshya170@gmail.com>
2022-06-13 01:04:15 +0530

    Connected to mongoDB database

--- server/app.js
+++ server/app.js
@@ -1,12 +1,27 @@
 const express = require("express")
 const app = express()
+const mongoose = require("mongoose")
 const PORT = 5000
+const { MONGOURI } = require("./keys")
 
-const customMiddleware = () => {
-  console.log("middleware executed")
+mongoose.connect(MONGOURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+mongoose.connection.on("connected", () => {
+  console.log("Connected to mongoDB")
+})
+
+const customMiddleware = (req, res, next) => {
+  console.log("middleware executed!")
+  next()
 }
 
+app.use(customMiddleware)
+
 app.get("/", (req, res) => {
+  console.log("home")
   res.send("Hello World")
 })
 
