23edb1fe696805df9f7e77b2f4069ac8b90166bb
Kumar Lakshya <klakshya170@gmail.com>
2022-06-14 00:41:04 +0530

    Created Midlleware for JWT authentication

---
+++ server/middleware/requireLogin.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken")
+const { JWT_SECRET } = require("../keys")
+const mongoose = require("mongoose")
+const User = mongoose.model("User")
+
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers
+  if (!authorization) {
+    res.status(401).json({ error: "You must be logged in" })
+  }
+  authorization.replace("Bearer ", "")
+  jwt.verify(token, JWT_SECRET, (err, payload) => {
+    if (err) {
+      return req.status(401).json({ error: "You must be logged in" })
+    }
+    const { _id } = payload.id
+    User.findById(_id).then((userData) => {
+      req.user = userdata
+    })
+    next()
+  })
+}
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,10 +3,17 @@ const router = express.Router()
 const mongoose = require("mongoose")
 const User = mongoose.model("User")
 const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+const { JWT_SECRET } = require("../keys")
+const requireLogin = require("../middleware/requireLogin")
 
 // router.get("/", (req, res) => {
 //   res.send("hello")
 // })
+
+router.get("/protected", requireLogin, (req, res) => {
+  res.send("Hello User")
+})
 
 router.post("/signup", (req, res) => {
   //   console.log(req.body.name)
@@ -42,4 +49,30 @@ router.post("/signup", (req, res) => {
     })
 })
 
+router.post("/signin", (req, res) => {
+  const { email, password } = req.body
+
+  if (!email || !password) {
+    res.status(422).json({ error: "Please add email or password" })
+  }
+  User.findOne({ email: email }).them((savedUser) => {
+    if (!savedUser) {
+      res.status(422).json({ error: "Invalid email or password" })
+    }
+    bcrypt
+      .compare(password, savedUser.password)
+      .then((doMatch) => {
+        if (doMatch) {
+          //   res.json({ message: "Signed in successfully" })
+          const token = jwt.sign({ _id: savedUser.id }, JWT_SECRET)
+          res.json({ token })
+        } else {
+          res.status(422).json({ error: "Invalid email or password" })
+        }
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  })
+})
 module.exports = router
