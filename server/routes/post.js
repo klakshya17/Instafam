6729fc87af8d4ebd7b0030c52785ee862b169aa5
Kumar Lakshya <klakshya170@gmail.com>
2022-06-14 01:54:57 +0530

    Added Node APIs for posts

--- server/routes/post.js
+++ server/routes/post.js
@@ -4,6 +4,17 @@ const mongoose = require("mongoose")
 const requireLogin = require("../middleware/requireLogin")
 const Post = mongoose.model("Post")
 
+router.get("/allpost", (req, res) => {
+  Post.find()
+    .populate("postedBy", "_id name")
+    .then((posts) => {
+      res.json({ posts })
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+})
+
 router.post("/createpost", requireLogin, (req, res) => {
   const { title, body } = req.body
   if (!title || !body) {
@@ -25,4 +36,16 @@ router.post("/createpost", requireLogin, (req, res) => {
       console.log(err)
     })
 })
+
+router.get("/mypost", (req, res) => {
+  Post.find({ postedBy: req.user._id })
+    .populate("postedBy", "_id name")
+    .then((mypost) => {
+      res.json(mypost)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+})
+
 module.exports = router
